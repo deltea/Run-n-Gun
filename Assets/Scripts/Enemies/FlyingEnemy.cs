640572870d8ba6e504f877a886a877a55a0a5bfd
thcheetah777 <thcheetah777@gmail.com>
2023-03-06 18:23:32 +0900

    Be alerted and add groupsof bullets

--- Assets/Scripts/Enemies/FlyingEnemy.cs
+++ Assets/Scripts/Enemies/FlyingEnemy.cs
@@ -9,6 +9,11 @@ public class FlyingEnemy : MonoBehaviour
     public float abovePlayerOffset = 1;
     public EnemyBullet bulletPrefab;
     public float delay = 1;
+    public float groupDelay = 0.1f;
+    public bool alerted = false;
+    public float alertRange = 5;
+    public int group = 3;
+    public LayerMask playerLayer;
 
     Transform player;
     Rigidbody2D enemyBody;
@@ -20,20 +25,40 @@ public class FlyingEnemy : MonoBehaviour
         StartCoroutine(ShootRoutine());
     }
 
-    void FixedUpdate() {
-        Vector3 abovePlayer = player.position + Vector3.up * abovePlayerOffset;
-        Vector2 direction = abovePlayer - transform.position;
-        enemyBody.AddForce(speed * direction);
+    void Update() {
+        if (alerted)
+        {
+            Vector3 abovePlayer = player.position + Vector3.up * abovePlayerOffset;
+            Vector2 direction = abovePlayer - transform.position;
+            enemyBody.AddForce(speed * direction);
+        } else
+        {
+            alerted = Physics2D.OverlapCircle(transform.position, alertRange, playerLayer);
+        }
     }
 
     private IEnumerator ShootRoutine() {
         while (true)
         {
-            yield return new WaitForSeconds(delay);
-            Vector2 direction = player.position - transform.position;
-            Instantiate(bulletPrefab.gameObject, transform.position, Quaternion.LookRotation(Vector3.forward, Quaternion.identity * direction));
+            if (alerted)
+            {
+                yield return new WaitForSeconds(delay);
+                for (int i = 0; i < group; i++)
+                {
+                    Vector2 direction = player.position - transform.position;
+                    Instantiate(bulletPrefab.gameObject, transform.position, Quaternion.LookRotation(Vector3.forward, Quaternion.identity * direction));
+                    yield return new WaitForSeconds(groupDelay);
+                }
+            } else
+            {
+                yield return null;
+            }
         }
 
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(transform.position, alertRange);
     }
 
 }
